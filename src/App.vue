98d7ac42f9cdc7307d5f293c22f65f3316c47945
AlexGONG123 <914434534@qq.com>
2023-12-28 09:08:23 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="btn">add user</button>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+.btn{
+  @apply bg-purple-500 text-indigo-50 px-4 py-2 rounded-full transition-all duration-500 hover:( bg-purple-900) focus:(ring-2 ring-purple-900);
+  position: absolute; /* 绝对定位 */
+  top: 10px; /* 顶部间距 */
+  left: 10px;
+}
+</style>
